a8af32759acc1b39b9294fa503b3bffc380d863b
Rabinkhulimuli <131673248+Rabinkhulimuli@users.noreply.github.com>
2024-01-18 21:42:02 +0545

    some changes

--- src/profilePic.jsx
+++ src/profilePic.jsx
@@ -2,16 +2,16 @@ function ProfilePic(){
     return (
         <nav className='headbox'>
                 <header className='head--pic'>
-                        <img className="big-profile" src="https://w0.peakpx.com/wallpaper/462/54/HD-wallpaper-one-piece-boa-hancock.jpg" alt="" />
+                        <img className="big-profile" src="../image/R-k.jpeg" alt="" />
                         <div className="small-pp">
-                                <img className='profile--pic' src="https://e1.pxfuel.com/desktop-wallpaper/238/852/desktop-wallpaper-masque-luffy-smiling-luffy-smile-thumbnail.jpg"/>
+                                <img className='profile--pic' src="../image/Rkimg.jpeg"/>
                                 <div className='pdetail'>
-                                    <h3 className='name'> Boa Hancok</h3>           
-                                    <small className='det'>Founder, Insight</small>
+                                    <h3 className='name'>RABIN KHULIMULI</h3>           
+                                    <small className='det'>Founder, Insight</small> 
                                 </div>
                         </div>
                 </header>
-                <p className='subdet'>A woman with strong desire to change the world.Thanks for connecting</p>
+                <p className='subdet'>A man with strong desire to change the world.Thanks for connecting</p>
         </nav>
     )
 }
