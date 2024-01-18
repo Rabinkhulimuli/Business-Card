ac387dce0efe73b87f50c5cab1cb3a445a9acc94
Rabin Khulimuli <131673248+Rabinkhulimuli@users.noreply.github.com>
2024-01-18 21:58:02 +0545

    Update components.jsx
    Some added

--- src/components.jsx
+++ src/components.jsx
@@ -3,11 +3,11 @@ export default function MainButton(){
         <main>
         <button className="btn-com" >
         <img className='phone' src="https://w7.pngwing.com/pngs/827/444/png-transparent-blue-phone-illustration-iphone-telephone-logo-computer-icons-contact-blue-electronics-telephone-call.png"/>
-        <p className='datadet'>Reach me on Phone</p>
+        <a className='datadet' href="https://t.snapchat.com/pfV2Mfki">Reach me on Phone</a>
         </button>
         <button className="btn-com" >
         <img className="phone" src="https://png.pngtree.com/png-clipart/20190613/original/pngtree-instagram-icon-logo-png-image_3560504.jpg"/>
-        <p className="datadet">Follow me on IG</p>
+        <a className="datadet" href="https://www.instagram.com/rabin_kh12?igsh=OXI5aHJ0ZTFzNjNm&utm_source=qr">Follow me on IG</a>
         </button>
         <button className="btn-com" >
         <img className="phone" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJ63xmdyqloMwBqqPbu6O9Fq1CX-Tdjq8dww&usqp=CAU"/>
@@ -15,11 +15,12 @@ export default function MainButton(){
         </button>
         <button className="btn-com" >
         <img className="phone" src="https://cdn3d.iconscout.com/3d/free/preview/tiktok-4727413-3925996@0.png?f=webp&h=700"/>
-        <p className="datadet" >View my Tiktok videos</p>
+        <a className="datadet" href="https://www.tiktok.com/@rabin_khulimuli?_r=1&_d=eb5fd0j40df08k&sec_uid=MS4wLjABAAAAAc7IfQ_Bj8D3f9GK_o0GfDeVu0d0AfuddxPydYTrd8BECgoQre5frUmCJHPBCO6c&share_author_id=7187408270737540123&sharer_language=en&source=h5_m&u_code=e5m8i5ca9eb77f&ug_btm=b8727,b0&social_share_type=4&utm_source=copy&sec_user_id=MS4wLjABAAAAAc7IfQ_Bj8D3f9GK_o0GfDeVu0d0AfuddxPydYTrd8BECgoQre5frUmCJHPBCO6c&tt_from=copy&utm_medium=ios&utm_campaign=client_share&enable_checksum=1&user_id=7187408270737540123&share_link_id=A7393603-8C5D-4CEB-B69D-F3A8C8CE2510&share_app_id=1233" >View my Tiktok videos</a>
         </button>
         <button className="btn-com" >
         <img className="phone" src="https://e7.pngegg.com/pngimages/125/937/png-clipart-youtube-logo-youtube-angle-logo.png"/>
-        <p className="datadet" >View my Youtube videos</p>
+        <a className="datadet" href="https://youtube.com/@aeror5579?si=gC1A98L8awG-rqqA"
+            >View my Youtube videos</a>
         </button>
         </main>
     )
